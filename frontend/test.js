b3fa6ac48e54821ff3624ac5b3769ed6468b4bb7
AKSHAY H <akshayh2004ak08@gmail.com>
2026-06-29 16:43:30 +0530

    front end components

---
+++ frontend/test.js
@@ -0,0 +1,55 @@
+// console.log("answer:");
+// let x = 10;
+// console.log(x);
+// const y = 20;
+// console.log(y);
+// console.log(x + y);
+// console.log(x+y);
+// function add(x, y) { 
+//     return x + y;
+// }
+// console.log(add(5, 10));
+// const arr = [1, 2, 3, 4, 5];
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+
+// dictionary
+// const dict = {"name": "John", "age": 30, "city": "New York"};
+// console.log(dict.name);
+// console.log(dict.age);
+// console.log(dict.city);
+// destructuring
+// let {name, age, city} = dict;
+// console.log(name);
+// console.log(age);
+// console.log(city);
+
+// destructuring of array
+// const arr2 = [1, 2, 3, 4, 5];
+// let [a, b, c, d, e] = arr2;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+
+// rest and spread operator
+
+// let a=[10,20];
+// let b=[30,40];
+// console.log(d=[...a,...b])
+// console.log([...a,...b]);
+
+// function add(a, ...b){
+//     return a+b;
+// }
+// console.log(add(10,20));
+// console.log(add(10,20,30,));
+// console.log(add(10,20,30,40,50));
+
+const a =10;
+const b =20;
+console.log(`The sum of ${a} and ${b} is ${a+b}`);
