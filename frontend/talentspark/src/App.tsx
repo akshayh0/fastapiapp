7c13f632e1d2b75401b7883a0c2efc7cbede10e4
AKSHAY H <akshayh2004ak08@gmail.com>
2026-06-30 12:37:37 +0530

    app.tsx

--- frontend/talentspark/src/App.tsx
+++ frontend/talentspark/src/App.tsx
@@ -3,14 +3,43 @@ import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import CompanyCard from "./components/CompanyCard";
 import JobCard from "./components/JobCard";
+import { useEffect, useState } from "react";
+import {getCompanies} from "./Services/companyService";
+import type { company } from "./types/company";
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+  const [companies, setCompanies] = useState<company[]>([]);
+
+  async function fetchCompanies() {
+    setLoading(true);
+    try {
+      const companies = await getCompanies();
+      setCompanies(companies);
+    } catch (error) {
+      setError(error as Error);
+    } finally {
+      setLoading(false);
+    }
+  }
+  useEffect(() => {
+    fetchCompanies();
+  }, []);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
   return (
     <>
     <NavBar />
     <Welcome />
     <Footer/>
-    <CompanyCard/>
+    <CompanyCard key={companies.id} 
+    companies={companies}/>
     <JobCard/>
+    <Footer/>
     </>
   )
 }
---
+++ frontend/talentspark/src/types/job.ts
@@ -0,0 +1,8 @@
+interface job{
+    id:number;
+    title:string;
+    salary:string;
+    description:string;
+    company_id:number;
+}
+export type {job}
