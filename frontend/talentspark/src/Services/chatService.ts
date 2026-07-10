1088bf8671546cbfe246e30fd01c6fc4132cb7a1
AKSHAY H <akshayh2004ak08@gmail.com>
2026-07-10 09:24:17 +0530

    deploy

--- frontend/talentspark/src/Services/chatService.ts
+++ frontend/talentspark/src/Services/chatService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { ChatRequest, ChatResponse } from "../types/chat";
 
-const API_URL = "http://127.0.0.1:8000/chat";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const sendMessage = async (
   data: ChatRequest
